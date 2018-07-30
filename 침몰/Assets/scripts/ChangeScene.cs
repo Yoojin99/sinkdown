d1cb6f949f2a548178d37dc5c1234fd9568d6d79
sojung127 <ssojung127@gmail.com>
2018-07-30 17:45:27 +0900

    changescene upadate

--- 침몰/Assets/scripts/ChangeScene.cs
+++ 침몰/Assets/scripts/ChangeScene.cs
@@ -5,15 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
-	// Use this for initialization
-	//void Start () {
-		
-	//}
 	
-	// Update is called once per frame
-	//void Update () {
-		
-	//}
 
     public void SceneChange()
     {
