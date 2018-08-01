d752db4ab438b237badc961f229619008a40b1b5
김혜지 <kimhj5854@naver.com>
2018-08-02 03:31:38 +0900

    Dialogue 초기 작업
    - 대화 구현 초기 작업입니다.
    - 1인대화
    - 버튼 클릭시 넘어감
    - UI 정리는 안 돼있어요.. ㅜㅜ 보기좋게 정리해놓겠습니당!
    
    > 2인대화 구현
    > 이미지와 함께 연동되는것 구현
    >

---
+++ 침몰/Assets/script/DialogueManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//이 클래스는 캐릭터의 말들을 입력할 수 있다.
+public class DialogueManager : MonoBehaviour
+{
+    public Queue<string> sentences; //대화 내용 
+    public Text nameText;//대화창: 이름
+    public Text dialogueText;//대화창 : 대화
+
+ 
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void StartDialogue(Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+}
