095bc4c2c88fe8bd4fb14208efb281c79299b778
Yoojin99 <crm05265@gmail.com>
2018-08-02 13:38:57 +0900

    08.02 아침

---
+++ 침몰/Assets/script/PoisonTrap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoisonTrap : MonoBehaviour {
+
+    private Player player;
+    Rigidbody2D rb;
+
+    public GameObject poisontrap;
+    public float SpawningTime = 2;
+    float nextSpawnTime;
+    Vector2 pos;
+    
+
+	// Use this for initialization
+	void Start () {
+        pos = this.gameObject.transform.position;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    //&& Time.time>nextSpawnTime
+    // Update is called once per frame
+    void Update () {
+        nextSpawnTime = Time.time + SpawningTime;
+	}
+    void OnTriggerEnter2D(Collider2D poison)
+    {
+        if (poison.CompareTag("Player"))
+        {
+            player.DealPoison(20);
+        }
+
+        if (poison.CompareTag("Untagged"))
+        {
+            poisontrap.transform.position = pos;
+            rb.isKinematic = true;
+        }
+    }
+    
+
+    }
